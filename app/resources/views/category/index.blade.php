94852dee33a8eadff5aaf4bb355fdd68d461cf48
Romera Jorge <romera.gomez.jorge@gmail.com>
2023-10-23 16:24:37 -0300

    View: improve title description in lists

--- app/resources/views/category/index.blade.php
+++ app/resources/views/category/index.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <div class="flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
             <h2 class="text-xl font-semibold leading-tight">
-                {{ __('Category') }}
+                {{ __('Categories') }}
             </h2>
             <x-buttons.create :href="route('categories.create')"/>
         </div>
