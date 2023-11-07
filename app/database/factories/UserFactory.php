58b153a4c29076f253f33a09fe273d7bd41e8869
Romera Jorge <romera.gomez.jorge@gmail.com>
2023-11-07 13:31:26 -0300

    User Seeder: set default user

--- app/database/factories/UserFactory.php
+++ app/database/factories/UserFactory.php
@@ -18,8 +18,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name'              => 'Jorge Romera',
-            'email'             => 'romera.gomez.jorge@gmail.com',
+            'name'              => 'Admin',
+            'email'             => 'admin@example.com',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
